8c64411846a1031a257f6406d775733096336e3b
Dr Nic Williams <drnicwilliams@gmail.com>
2009-11-27 03:45:16 +1000

    support cucumber env

--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
-if RAILS_ENV =~ /^test/ || RAILS_ENV =~ /selenium/
+if RAILS_ENV =~ /^test/ || RAILS_ENV =~ /selenium/ || RAILS_ENV =~ /cucumber/
   require 'mainline'
   require 'mainline/connection_pool'
   require 'mainline/integration_test'
